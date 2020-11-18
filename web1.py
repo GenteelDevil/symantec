e4cc98be21f316ecb0a7b7f6c07f1ffa56ebeb6f
OneShell <onshell0503@gmail.com>
2020-11-18 17:20:52 +0800

    admin click over

--- web1.py
+++ web1.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
+from re import match
 import requests
 import urllib
 import time
 import sys
 import re
+import json
 from bs4 import BeautifulSoup
 
 #login_url="https://"+ +"/console/apps/sepm"
@@ -12,7 +14,7 @@ from bs4 import BeautifulSoup
 login_username=""
 login_password=""
 
-
+main_frame = ""
 #default setting
 server_ip = "172.16.226.20"
 default_url_local="https://%s:8443/console/apps/sepm?do" % (server_ip)
@@ -40,18 +42,18 @@ proxies = {
 
 def get_login_sessions():
     #1 get csrfToken and cookies
-    req_1= s.get(default_url_local,proxies=proxies,cookies=cookies,headers=headers,verify=False)
+    req_1= s.get(default_url_local,proxies={}, cookies=cookies,headers=headers,verify=False)
     soup = BeautifulSoup(req_1.text, 'html.parser')
     csrfToken = soup.find('input', {'id':'csrfToken'})["value"]
     csrfToken=csrfToken.encode("utf-8")
     #2 get init_js
     init_js_url="https://%s:8443/console/scripts/ajaxswing/AjaxSwing_init.js" % server_ip
-    req_2 = s.get(init_js_url,proxies=proxies,headers=headers,verify=False)
+    req_2 = s.get(init_js_url,proxies={},headers=headers,verify=False)
     for i in req_1.headers["Set-Cookie"].split(";"):
         if "JSESSIONID"  in i :
             JSESSIONID=i
             JSESSIONID=JSESSIONID.encode("utf-8")
-            print JSESSIONID
+            #print JSESSIONID
 
     #3 get calibrate
     calibrate_data={
@@ -59,8 +61,8 @@ def get_login_sessions():
         'AjaxSwingInitData':'864,969,480,480,480,0,0',
     }
     calibrate_data["csrfToken"]=csrfToken
-    req_3=s.post(default_url_local,proxies=proxies,data=calibrate_data,headers=headers,verify=False)
-    req_3_2=s.post(default_url_local,proxies=proxies,data=calibrate_data,headers=headers,verify=False)
+    req_3=s.post(default_url_local,proxies={},data=calibrate_data,headers=headers,verify=False)
+    req_3_2=s.post(default_url_local,proxies={},data=calibrate_data,headers=headers,verify=False)
 
     #4 get FontCalibrationData
     FontCalibrationData={
@@ -68,7 +70,7 @@ def get_login_sessions():
         'FontCalibrationData':'couriernew,16,1;couriernew,12,1.5741;couriernew,12,1.2593;couriernew,12,1.2593;couriernew,12,1.0494;couriernew,12,1;couriernew,12,1;couriernew,13,1;couriernew,14,1;couriernew,15,1;arial,16,1.0191;arial,12,1.6667;arial,12,1.6162;arial,12,1.2698;arial,12,1.1348;arial,12,1.1429;arial,12,1;arial,13,1;arial,14,1;arial,15,1.0309;timesnewroman,16,1.2682;timesnewroman,12,1.9101;timesnewroman,12,1.7;timesnewroman,12,1.4655;timesnewroman,12,1.405;timesnewroman,12,1.2143;timesnewroman,12,1.1888;timesnewroman,13,1.172;timesnewroman,14,1.1786;timesnewroman,15,1.2102;::couriernew,16,1.0279;couriernew,12,1.9302;couriernew,12,1.5442;couriernew,12,1.5442;couriernew,12,1.2868;couriernew,12,1.103;couriernew,12,1.103;couriernew,13,1.0436;couriernew,14,1.125;couriernew,15,1.0724;arial,16,1.0116;arial,12,1.7031;arial,12,1.5139;arial,12,1.2925;arial,12,1.1474;arial,12,1.1848;arial,12,1.0219;arial,13,1;arial,14,1.0133;arial,15,1.0225;timesnewroman,16,1.0425;timesnewroman,12,1.7754;timesnewroman,12,1.5091;timesnewroman,12,1.302;timesnewroman,12,1.2251;timesnewroman,12,1.0885;timesnewroman,12,1;timesnewroman,13,1.0497;timesnewroman,14,1.0459;timesnewroman,15,1.0197;'
     }
     FontCalibrationData["csrfToken"]=csrfToken
-    req_4=s.post(default_url_local,proxies=proxies,data=FontCalibrationData,headers=headers,verify=False)
+    req_4=s.post(default_url_local,proxies={},data=FontCalibrationData,headers=headers,verify=False)
 
 
     #5 login
@@ -79,7 +81,7 @@ def get_login_sessions():
         '__FastSubmit':'true',
     }
     login_data_1["__csrfToken"]=csrfToken
-    req_login_1=s.post(default_url_local_2,data=login_data_1,proxies=proxies,headers=headers,verify=False)
+    req_login_1=s.post(default_url_local_2,data=login_data_1,proxies={},headers=headers,verify=False)
     
 
     if "componentId" in req_login_1.text:
@@ -89,7 +91,8 @@ def get_login_sessions():
                 if "_" in j :
                     componentId=j
                     componentId=componentId.encode("utf-8")
-                    print componentId
+                    main_frame = componentId
+                    # print componentId
     else :
         print "not find componentId"
     # get componentId
@@ -100,7 +103,7 @@ def get_login_sessions():
     login_data_2["__csrfToken"]=csrfToken
     login_data_2["actionString"]="/noupdate/"+componentId+"/"
     login_data_2["storedActions[]"]="/type/"+componentId+"/a"
-    req_login_2=s.post(default_url_local_2,data=login_data_2,proxies=proxies,headers=headers,verify=False)
+    req_login_2=s.post(default_url_local_2,data=login_data_2,proxies={},headers=headers,verify=False)
     time.sleep(2)
     #get focusedComponentId and SEPMPasswordField
     if "focusedComponentId" in req_login_2.text:
@@ -110,13 +113,13 @@ def get_login_sessions():
                     if "JTextField" in j:
                         JTextField=j
                         JTextField=JTextField.encode("utf-8")
-                        print JTextField
+                        # print JTextField
             if "SEPMPasswordField" in i:
                 for j in i.split("'"):
                     if "SEPMPasswordField" in j:
                         SEPMPasswordField=j
                         SEPMPasswordField=SEPMPasswordField.encode("utf-8")
-                        print SEPMPasswordField
+                        # print SEPMPasswordField
     else:
         print "not find focusedComponentId"
 
@@ -128,7 +131,7 @@ def get_login_sessions():
     login_data_3["__csrfToken"]=csrfToken
     login_data_3["actionString"]="/keydown/"+JTextField+"/n_9_1"
     login_data_3["storedActions[]"]="/type/"+JTextField+"/admin"
-    req_login_3=s.post(default_url_local_2,data=login_data_3,proxies=proxies,headers=headers,verify=False)
+    req_login_3=s.post(default_url_local_2,data=login_data_3,proxies={},headers=headers,verify=False)
     time.sleep(2)
 
     login_data_4={
@@ -138,10 +141,11 @@ def get_login_sessions():
     login_data_4["__csrfToken"]=csrfToken
     login_data_4["actionString"]="/keydown/"+SEPMPasswordField+"/n_13_1"
     login_data_4["storedActions[]"]="/type/"+SEPMPasswordField+"/Admin@123"
-    login_data_4=s.post(default_url_local_2,data=login_data_4,proxies=proxies,headers=headers,verify=False)
+    login_data_4=s.post(default_url_local_2,data=login_data_4,proxies={},headers=headers,verify=False)
     time.sleep(10)
 
-    login_data_5= s.get(default_url_local_2,proxies=proxies,headers=headers,verify=False)
+    login_data_5= s.get(default_url_local_2,headers=headers,verify=False)
+    #print login_data_5.text
     if "PHPSESSID" in login_data_5.text:
         soup = BeautifulSoup(login_data_5.text, 'html.parser')
         PHPSESSID_url = soup.find('iframe')["src"]
@@ -153,20 +157,13 @@ def get_login_sessions():
     else :
         print "not find PHPSESSID!"
     
-    login_data_6=s.get(PHPSESSID_url,proxies=proxies,headers=headers,verify=False)
-
-    print "cookieTest=true;%s;ssc=1;%s"%(JSESSIONID,PHPSESSID)
+    login_data_6=s.get(PHPSESSID_url,headers=headers,verify=False)
+    print "cookies info:"
+    print "  cookieTest=true;%s;ssc=1;%s"%(JSESSIONID,PHPSESSID)
+    print "BodyInfo:"
+    print "  csrfToken=%s" % csrfToken
+    print "  MainFrameID=%s" % main_frame
     return csrfToken,componentId
-
-'''
-def init_do():
-    post_url_local="https://192.168.20.1:8443/console/apps/sepm?do" 
-    data={
-        'calibrate':'true',
-        'AjaxSwingInitData':'864%2C969%2C480%2C480%2C480%2C0%2C0',
-        'csrfToken':'%s',
-    }%csrfToken
-'''
 
 def keep_alive(csrfToken):
     post_url = "https://%s:8443/console/apps/sepm" % server_ip
@@ -178,17 +175,116 @@ def keep_alive(csrfToken):
     }
     s.post(post_url, data=data, headers=headers, verify=False)
 
-def upload_packages(csrfToken,componentId):
-    print s.cookies
-    while(True):
-        time.sleep(1)
-        keep_alive(csrfToken=csrfToken)
+def upload_file(csrfToken):
+    post_url = "https://%s:8443/console/apps/sepm" % server_ip
+    # get QuickStart window ID
+
+    data = {
+        'actionString' : '/click/QuickStartDialog_1305348633/562_707#n',
+        '__Action' : 'v4',
+        '__FastSubmit' : 'true',
+        '__csrfToken' : csrfToken
+    }
+    response_json = s.post(post_url, data=data, headers=headers, verify=False).text
+    quick_start_dialog_id = json.loads(response_json)['activeWindowId']
+    print quick_start_dialog_id
+
+    # 0. close start window
+    print "0. close start window"
+    data = {
+        'actionString' : '/click/%s/562_707#n' % quick_start_dialog_id,
+        '__Action' : 'v4',
+        '__FastSubmit' : 'true',
+        '__csrfToken' : csrfToken
+    }
+    response_json = s.post(post_url, data=data, headers=headers, verify=False).text
+    main_frame_id = json.loads(response_json)["activeWindowId"]
+
+    # 1. click admin
+    print "1. click Admin"
+    # print main_frame_id
+    body_data1 = {
+        'actionString' : '/click/%s/31_453#n' % main_frame_id,
+        '__Action' : 'v4',
+        '__FastSubmit' : 'true',
+        '__csrfToken' : '%s' % csrfToken
+    }
+    s.post(post_url, data=body_data1, headers=headers, verify=False)
+
+    print "2. click install package"
+    data = {
+        'actionString' : '/click/%s/113_897#n' % main_frame_id,
+        '__Action' : 'v4',
+        '__FastSubmit' : 'true',
+        '__csrfToken' : csrfToken
+    }
+    s.post(post_url, data=data, proxies=proxies, headers=headers, verify=False)
+    time.sleep(2)
+
+    print "3. click add package"
+
+    data = {
+        'actionString' : '/click/%s/164_615#n' % main_frame_id,
+        '__Action' : 'v4',
+        '__FastSubmit' : 'true',
+        '__csrfToken' : csrfToken
+    }
+
+    data_init = {
+        'actionString' : '/context/%s/1_1#n' % main_frame_id,
+        '__Action' : 'v4',
+        '__FastSubmit' : 'true',
+        '__csrfToken' : csrfToken
+    }
+    s.post(post_url, data=data, proxies=proxies, headers=headers, verify=False).text
+    response_json = s.post(post_url, data=data_init, proxies=proxies, headers=headers, verify=False).text
+    creat_new_software_dlg_id = json.loads(response_json)['activeWindowId']
+    jtext_field_id = json.loads(response_json)['focusedComponentId']
 
 
+    print jtext_field_id
+    print creat_new_software_dlg_id
+
+    '''
+    print "4. input package name"
+    # click input box
+    data = {
+        'actionString' : '/click/%s/62_78#n' % creat_new_software_dlg_id,
+        'postActions[]' : '/caret/%s/0' % jtext_field_id,
+        '__Action' : 'v4',
+        '__FastSubmit' : 'true',
+        '__csrfToken' : csrfToken
+    }
+
+    # input package name
+    data = {
+        'actionString' : '/noupdate/%s/' % jtext_field_id,
+        'storedActions[]' : '/type/%s/thisisatestpackage' % jtext_field_id,
+        '__Action' : 'v4',
+        '__FastSubmit' : 'true',
+        '__csrfToken' : csrfToken
+    }
+    s.post(post_url, data=data, proxies=proxies, headers=headers, verify=False)
+
+    print "5. create select file windows"
+    data = {
+        'actionString' : '/click/%s/293_132#n' % jtext_field_id,
+        'storedActions[]' : '/type/%s/thisisatestpackage' % jtext_field_id,
+        '__Action' : 'v4',
+        '__FastSubmit' : 'true',
+        '__csrfToken' : csrfToken
+    }
+    s.post(post_url, data=data, proxies=proxies, headers=headers, verify=False)
+
+
+
+
+'''
     
 if __name__ == '__main__':
     csrfToken,componentId=get_login_sessions()
-    upload_packages(csrfToken,componentId)
+    upload_file(csrfToken=csrfToken)
+    while True:
+        time.sleep(1)
+        keep_alive(csrfToken=csrfToken)
     
-
-
